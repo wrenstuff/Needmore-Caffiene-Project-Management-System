e6d33072a95f9e679677f50c5f1283280a7b3f11
wrenstuff <stewart.nzwren@gmail.com>
2025-05-22 10:17:59 +1200

    v0.1.1
    Navigation bar had been created and displays in all pages except for login.html and sign_up.html as it's not required in those.

--- components/nav_bar.js
+++ components/nav_bar.js
@@ -8,7 +8,19 @@ connectedCallback() {
     // stylesheet for the component can be found at styles/nav-bar.scss
     this.innerHTML = 
     `
-        <h1>NAV BAR (MODIFY IN components/nav-bar.js)</h1>
+        <div class="nav-bar">
+        <ul class="nav-left">
+            <li><a href="dashboard.html">Home</a></li>
+            <li><a href="projects.html">Projects</a></li>
+            <li><a href="marketplace.html">Marketplace</a></li>
+            <li><a href="pricing.html">Membership</a></li>
+            <li><a href="about_us.html">About Us</a></li>
+            <li><a href="faq.html">FAQ</a></li>
+        </ul>
+        <ul class="nav-right">
+            <li><a href="account.html">Account</a></li>
+        </ul>
+    </div>
     `
     }
 
