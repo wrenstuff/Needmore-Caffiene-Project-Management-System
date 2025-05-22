5d1725245d6419c58e1153ce9e6e8916fccd8376
wrenstuff <stewart.nzwren@gmail.com>
2025-05-23 10:58:21 +1200

    v0.1.2
    Footer is currently in a state I am happy with. All it needs now is the social media icons, and for all of the links to be directed to the correct places.

--- components/footer.js
+++ components/footer.js
@@ -8,36 +8,36 @@ connectedCallback() {
     // stylesheet for the component can be found at styles/footer.scss
     this.innerHTML = 
     `
-        <div class="page-footer">
-    <div class="social-media-links">
-        <ul>
-            <li><a href="https://www.facebook.com" target="_blank">Facebook</a></li>
-            <li><a href="https://www.twitter.com" target="_blank">Twitter</a></li>
-            <li><a href="https://www.instagram.com" target="_blank">Instagram</a></li>
-            <li><a href="https://www.linkedin.com" target="_blank">LinkedIn</a></li>
-            <li><a href="https://www.youtube.com" target="_blank">YouTube</a></li>
-            <li><a href="https://www.gmail.com" target="_blank">Gmail</a></li>
-        </ul>
+    <div class="page-footer">
+        <div class="social-media-links">
+            <ul>
+                <li><a href="https://www.facebook.com" target="_blank"><img class="social-icons" src="../images/icon-placeholder.png" alt="facebook"></a></li>
+                <li><a href="https://www.twitter.com" target="_blank"><img class="social-icons" src="../images/icon-placeholder.png" alt="twitter"></a></li>
+                <li><a href="https://www.instagram.com" target="_blank"><img class="social-icons" src="../images/icon-placeholder.png" alt="instagram"></a></li>
+                <li><a href="https://www.linkedin.com" target="_blank"><img class="social-icons" src="../images/icon-placeholder.png" alt="linkedin"></a></li>
+                <li><a href="https://www.youtube.com" target="_blank"><img class="social-icons" src="../images/icon-placeholder.png" alt="youtube"></a></li>
+                <li><a href="mailto:#" target="_blank"><img class="social-icons" src="../images/icon-placeholder.png" alt="email"></a></li>
+            </ul>
+        </div>
+        <div class="contact-info">
+            <ul>
+                <li><h3>Contact Us</h3></li>
+                <li><p><a href="#">123 Coffee Street</a> : Address</p></li>
+                <li><p><a href="#">+64 123 123 1234</a> : Phone</p></li>
+                <li><p><a href="mailto:#">email@email.com</a> : Email</p></li>
+    
+            </ul>
+        </div>
+        <div class="legal-info">
+            <ul>
+                <li><p><a href="#">Privacy Policy</a></p></li>
+                <li><p><a href="#">Terms of Service</a></p></li>
+            </ul>
+        </div>
+        <div class="copyright">
+            <p>&copy; 2025 Needmore Caffiene. All rights reserved.</p>
+        </div>
     </div>
-    <div class="contact-info">
-        <ul>
-            <li><h3>Contact Us</h3></li>
-            <li><p><a href="#">Banana Street</a> : Address</p></li>
-            <li><p><a href="#">+64 123 123 1234</a> : Phone</p></li>
-            <li><p><a href="mailto:#">email@email.com</a> : Email</p></li>
-
-        </ul>
-    </div>
-    <div class="legal-info">
-        <ul>
-            <li><p><a href="#">Privacy Policy</a></p></li>
-            <li><p><a href="#">Terms of Service</a></p></li>
-        </ul>
-    </div>
-    <div class="copyright">
-        <p>&copy; 2025 Needmore Caffiene. All rights reserved.</p>
-    </div>
-</div>
     `
     }
 
