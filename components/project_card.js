0a438b9115f257b1db4bd4dc5d9adfe6e9f4933a
wrenstuff <stewart.nzwren@gmail.com>
2025-05-27 11:53:06 +1200

    v0.1.8.1
    project card has been created however there's a slight issue with positioning with sizing due to the image

--- components/project_card.js
+++ components/project_card.js
@@ -3,12 +3,26 @@ class Project_Card extends HTMLElement {
         super();
     }
 
-connectedCallback() {
+    connectedCallback() {
+        const uniqueId = Date.now().toString(36) + Math.random().toString(36).substring(2);
 
-    // stylesheet for the component can be found at styles/card.scss
-    this.innerHTML = 
-    `
-        <h1>PROJECT CARD (MODIFY IN project-card.js)</h1>
+        // stylesheet for the component can be found at styles/card.scss
+        this.innerHTML =
+        `
+        <div class="project card" id="${uniqueId}">
+            <div class="notif">
+                <p>9+</p>
+            </div>
+            <div class="top">
+                <img src="../images/Blue_test.png" alt="">
+            </div>
+            <div class="bottom">
+                <!-- Project Name -->
+                <p class="name">Project: NAME</p>
+                <!-- Owned By -->
+                <p class="create">Owned By: NAME</p>
+            </div>
+        </div>
     `
     }
 
