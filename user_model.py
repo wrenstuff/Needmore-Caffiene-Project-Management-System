7f961a216b2672a953210ef9fba3136b46aabf76
wrenstuff <stewart.nzwren@gmail.com>
2025-06-24 14:56:33 +1200

    project create
    project create. This is unfortunately susceptible to HTML injections at the moment, however this is something that can be looked into at a later date

--- user_model.py
+++ user_model.py
@@ -3,9 +3,20 @@ from flask_login import UserMixin
 db = SQLAlchemy()
 
 # Define the User model with usermixin for Flask-Login integration
+class Project(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f'<Project {self.title}>'
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(32), unique=True, nullable=False)
     email = db.Column(db.String(254), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
     image_path = db.Column(db.String(255))
+    projects = db.relationship('Project', backref='owner', lazy=True)
